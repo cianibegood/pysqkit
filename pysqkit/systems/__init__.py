a1d3d0c7325ae235bdf9fad06c29882d01781d5d
Boris Varbanov <b.m.varbanov@gmail.com>
2021-03-24 10:50:42 +0100

    Implement general coupling term class

--- pysqkit/systems/__init__.py
+++ pysqkit/systems/__init__.py
@@ -1,3 +1,5 @@
 from . import coupledfluxqubit
+from .system import System
+from .couplers import CouplingTerm
 
-__all__ = ['coupledfluxqubit']
+__all__ = ['coupledfluxqubit', 'System', 'CouplingTerm']
