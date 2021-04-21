edbbaf98bfb9da8864bbf0e0039e79f37ac480c0
cianibegood <alessandrociani89@gmail.com>
2021-04-21 16:32:36 +0200

    Added solver for map in solvkit

--- docs/tutorial/coupled_qubits/coupled_fluxonia.py
+++ docs/tutorial/coupled_qubits/coupled_fluxonia.py
@@ -25,6 +25,8 @@ flx2 = fluxonium.Fluxonium('F2', ec, el[1], ej[1], flux[1], dim_hilbert=50)
 flx_sys = flx1.couple_to(flx2)
 
 # %%
-flx_sys.charge_op
+truncation = {'F1': 8, 'F2': 9}
+flx_sys.convert_subsys_operator('F1', 'CIAO', truncated_levels=truncation)
+
 
 # %%
--- docs/tutorial/rabi/rabi_oscillations.py
+++ docs/tutorial/rabi/rabi_oscillations.py
@@ -10,6 +10,7 @@ from matplotlib import rcParams
 rcParams['mathtext.fontset'] = 'cm'
 
 
+
 def plot_rabi(tlist, sz_avg):
     fs = 24
     ts = 14
@@ -23,6 +24,12 @@ def plot_rabi(tlist, sz_avg):
     plt.grid(linestyle = '--')
     plt.show()
 
+def pulse_shape(
+    t: float, 
+    args: dict
+    ) -> complex:
+    return args["w_rabi"]*np.cos(args["w_drive"]*t)
+
 # %%
 """ 
 Problem setup 
@@ -35,7 +42,6 @@ hamil0 = omega/2*qtp.sigmaz()
 drive = [qtp.sigmax()] #more drives can be added
 omega_d = 1.0 #drive frequency
 omega_rabi = 0.01 #by increasing this you clearly see non-RWA effects
-tlist = np.linspace(0, 3000, 10000)
 pulse = [omega_rabi*np.cos(omega_d*tlist)]
 gamma = 0.001 #relaxation rate
 jump_op = [np.sqrt(gamma)*qtp.sigmam()]
@@ -64,5 +70,53 @@ Plot
 """
 
 plot_rabi(tlist, sz_avg)
+
+# %%
+"""
+We construct the Hamiltonian as QobjEvo
+"""
+
+hamil = qtp.QobjEvo([hamil0, [drive[0], pulse_shape]], \
+    args={"w_rabi": omega_rabi, "w_drive": omega_d}, tlist=tlist)
+# To access hamil at a given time t
+hamil(t=100)
+
+#%%
+"""
+The Liouvillian can be constructed as
+"""
+liouv_rabi = qtp.liouvillian(hamil, jump_op)
+#liouv_rabi.compile()
+#To acces liouv_rabi at a given time t
+liouv_rabi(t=100)
+chi_map = (tlist[-1]*liouv_rabi(0)).expm()
+kraus_map = qtp.to_kraus(chi_map)
+rho_in = qtp.ket2dm(state_in)
+rho_f = kraus_map[0]*rho_in*kraus_map[0].dag() + \
+    kraus_map[1]*rho_in*kraus_map[1].dag()
+
+
+#%%
+"""
+Obtaining the CPTP map associated with the evolution. The map 
+is given in terms of Kraus operators evaluated at each time step. 
+"""
+
+rabi_map = solvkit.cptp_map(tlist, hamil0, drive, [pulse_shape], jump_op, \
+    args={"w_rabi": omega_rabi, "w_drive": omega_d})
+
+# %%
+"""
+Check that final state is consistent 
+"""
+rho_in = qtp.ket2dm(state_in)
+n_times = len(tlist) - 1
+# n_times = 0
+rho_f = rabi_map[n_times][0]*rho_in*rabi_map[n_times][0].dag() + \
+    rabi_map[n_times][1]*rho_in*rabi_map[n_times][1].dag() + \
+        rabi_map[n_times][2]*rho_in*rabi_map[n_times][2].dag() + \
+            rabi_map[n_times][3]*rho_in*rabi_map[n_times][3].dag()
+print(rho_f)
+
 
 # %%
