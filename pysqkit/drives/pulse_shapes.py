50411b54cb413bb29f53b54cadd3d98f3aa36cee
cianibegood <alessandrociani89@gmail.com>
2022-02-01 23:47:37 +0100

    Added integrated pulse to cr, results for cr look very good

--- pysqkit/drives/pulse_shapes.py
+++ pysqkit/drives/pulse_shapes.py
@@ -24,18 +24,19 @@ def gaussian_top(time: np.ndarray, rise_time: float, pulse_time: float, start_ti
     
     _start_times = time < start_time
     _rise_times = (time < start_time + rise_time)*(time >= start_time)
-    _fall_times = (time >= start_time + rise_time)*(time < pulse_time + start_time)
+    _drive_times = (time >= start_time + rise_time)*(time < start_time + pulse_time - rise_time)
+    _fall_times = (time >= start_time + pulse_time - rise_time)*(time < pulse_time + start_time)
     _end_times = time >= pulse_time + start_time
 
     if rise_time == 0:
         shape = np.ones(len(time))
-        shape[_start_times]=0.0
-        shape[_end_times]=0.0
+        shape[_start_times] = 0.0
+        shape[_end_times] = 0.0
         return shape
 
     shape = np.ones(len(time))
-    shape[_start_times]=0.0
-    shape[_end_times]=0.0
+    shape[_start_times] = 0.0
+    shape[_end_times] = 0.0
     shape[_rise_times] = gaussian_edge(time[_rise_times], rise_time)
     shape[_fall_times] = gaussian_edge(
         time[_fall_times] - (pulse_time - 2*rise_time), rise_time
