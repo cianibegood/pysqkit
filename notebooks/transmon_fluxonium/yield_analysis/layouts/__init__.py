2e10806f0d6cb29a0c784991a88ad242985220ca
b.m.varbanov <b.m.varbanov@gmail.com>
2022-04-26 18:07:15 +0200

    Update __init__.py
    Move freq assignment methods to transmon_util and include them in update

--- notebooks/transmon_fluxonium/yield_analysis/layouts/__init__.py
+++ notebooks/transmon_fluxonium/yield_analysis/layouts/__init__.py
@@ -2,4 +2,19 @@ from .layout import Layout
 from .library import surface_code
 from .plotter import plot
 
-__all__ = ["Layout", "surface_code", "plot"]
+from .transmon_util import (
+    set_freq_groups,
+    set_target_freqs,
+    sample_freqs,
+    get_collisions,
+)
+
+__all__ = [
+    "Layout",
+    "surface_code",
+    "plot",
+    "set_freq_groups",
+    "set_target_freqs",
+    "sample_freqs",
+    "get_collisions",
+]
