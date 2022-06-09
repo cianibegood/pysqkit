c9421548fc2cb8877bca2ca52042d1fa78948279
b.m.varbanov <b.m.varbanov@gmail.com>
2022-06-09 11:46:34 +0200

    Update fig_13_gen and data.
    Standardize yield result naming conventions between yield and number of collision scans. Update plot notebooks accordingly.

--- notebooks/transmon_fluxonium/paper_plots/fig_13/fig_13_gen.py
+++ notebooks/transmon_fluxonium/paper_plots/fig_13/fig_13_gen.py
@@ -26,16 +26,16 @@ import matplotlib.pyplot as plt
 
 # %%
 TEX_RC_PARAMS = {
-    'backend': 'ps',
+    "backend": "ps",
     #'text.latex.preamble': [r'\usepackage{gensymb}'],
-    'axes.labelsize': 10,
-    'axes.titlesize': 10,
-    'legend.fontsize': 10,
-    'xtick.labelsize': 8,
-    'ytick.labelsize': 8,
+    "axes.labelsize": 10,
+    "axes.titlesize": 10,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 8,
+    "ytick.labelsize": 8,
     #'text.usetex': True,
-    'font.family': 'serif',
-    'mathtext.fontset' : 'cm'
+    "font.family": "serif",
+    "mathtext.fontset": "cm",
 }
 
 # %%
@@ -57,7 +57,7 @@ MAX_RES_VAR = 0.1
 NUM_SEEDS = 6000
 DISTANCE = 3
 
-arr_name = f"mixed_device_yield_d_{DISTANCE}_resist_{MIN_RES_VAR}_{MAX_RES_VAR}_seeds_{NUM_SEEDS}_num_collisions_{AMP_LABEL}_drive.nc"
+arr_name = f"mixed_device_num_collisions_d_{DISTANCE}_resist_{MIN_RES_VAR}_{MAX_RES_VAR}_seeds_{NUM_SEEDS}_v3_{AMP_LABEL}_drive.nc"
 num_collisions = xr.load_dataarray(DATA_FOLDER / arr_name)
 
 NUM_COLLISION_TPYES = num_collisions.collision_ind.size
@@ -73,24 +73,21 @@ cmap = plt.cm.viridis
 colors = cmap(np.linspace(0, 1, 9))
 
 with plt.rc_context(TEX_RC_PARAMS):
-    fig, ax = plt.subplots(
-        figsize=(3.39, 2.6189190273276797), 
-        constrained_layout=True
-    )
-    
+    fig, ax = plt.subplots(figsize=(3.39, 2.6189190273276797), constrained_layout=True)
+
     avg_cols = num_collisions.mean(dim="seed")
-    
+
     for ind in range(NUM_COLLISION_TPYES):
-        type_cols = avg_cols.isel(collision_ind = ind)
+        type_cols = avg_cols.isel(collision_ind=ind)
         color = colors[ind]
-        
+
         label = r"$\mathrm{Type} %d$" % (ind + 1)
-        
+
         ax.bar(
-            np.log10(type_cols.resist_var), 
-            type_cols,  
-            width=BAR_WIDTH, 
-            bottom=bar_bottom, 
+            np.log10(type_cols.resist_var),
+            type_cols,
+            width=BAR_WIDTH,
+            bottom=bar_bottom,
             label=label,
             color=color,
             align="center",
@@ -107,32 +104,32 @@ with plt.rc_context(TEX_RC_PARAMS):
     ax.set_xticks(xtick_vals)
     ax.set_xticklabels(["$10^{%d}$" % val for val in xtick_vals])
     ax.set_xlabel("$\mathrm{Resistance} \, \mathrm{variation} \, \sigma_{R}/R$")
-    
+
     ax.legend(
-        frameon=False, 
+        frameon=False,
         ncol=1,
         bbox_to_anchor=(1.05, 1),
-        loc='upper left',
+        loc="upper left",
         handletextpad=0.3,
         labelspacing=0.3,
         borderpad=0.2,
         markerscale=1.0,
         handlelength=1.5,
         borderaxespad=0.05,
-        columnspacing=0.4
-    )    
-    
+        columnspacing=0.4,
+    )
+
     image_name = f"avg_num_col_vs_resist_var_d_{DISTANCE}_drive_amp_{AMP_VAL}"
     if SAVE_IMAGE:
-        for file_format in ('pdf', 'png'):
+        for file_format in ("pdf", "png"):
             full_name = f"{image_name}.{file_format}"
             fig.savefig(
-                IMG_FOLDER / full_name,  
-                dpi=300,  
-                bbox_inches='tight', 
-                transparent=True, 
+                IMG_FOLDER / full_name,
+                dpi=300,
+                bbox_inches="tight",
+                transparent=True,
                 format=file_format,
-                pad_inches=0
+                pad_inches=0,
             )
 plt.show()
 
