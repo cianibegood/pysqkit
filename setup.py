d6f9c898cf253ac99995eaaf9016d1a94cfb7ad4
b.m.varbanov <b.m.varbanov@gmail.com>
2022-06-09 18:01:58 +0200

    Update setup.py
    Update author email and update to uppercase style for constants.

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as f:
-    readme = f.read()
+with open("README.rst") as f:
+    README = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+with open("LICENSE") as f:
+    LICENSE = f.read()
 
 setup(
-    name='pysqkit',
-    version='0.0.1',
-    description='Python superconducting qubit circuit analysis package',
-    long_description=readme,
-    author='Alessandro Ciani',
-    author_email='a.ciani@tudelft.nl',
-    url='https://github.com/cianibegood/pysqkit',
-    license=license,
-    packages=find_packages('.'),
-    ext_package='pysqkit',
-    install_requires=list(open('requirements.txt').read().strip().split('\n'))
+    name="pysqkit",
+    version="0.0.1",
+    description="Python superconducting qubit circuit analysis package",
+    long_description=README,
+    author="Alessandro Ciani",
+    author_email="alessandrociani89@gmail.com",
+    url="https://github.com/cianibegood/pysqkit",
+    license=LICENSE,
+    packages=find_packages("."),
+    ext_package="pysqkit",
+    install_requires=list(open("requirements.txt").read().strip().split("\n")),
 )
