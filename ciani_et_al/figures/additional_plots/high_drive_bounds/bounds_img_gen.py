51a27e1f62842124952c6dcc0502a20a4b3d666b
b.m.varbanov <b.m.varbanov@gmail.com>
2022-10-03 17:45:41 +0200

    Update additional figures plot notebooks.

--- ciani_et_al/figures/additional_plots/high_drive_bounds/bounds_img_gen.py
+++ ciani_et_al/figures/additional_plots/high_drive_bounds/bounds_img_gen.py
@@ -6,11 +6,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.4.2
+#       jupytext_version: 1.4.0
 #   kernelspec:
-#     display_name: repetition_codes_env
+#     display_name: Python 3
 #     language: python
-#     name: repetition_codes_env
+#     name: python3
 # ---
 
 # %% [markdown]
